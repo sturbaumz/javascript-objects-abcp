b4e438f67638c58392fdbcabe13c92ec28e47941
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-15 07:08:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -14,6 +14,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
+  return playlist
 }
 
 removeFromPlaylist(playlist, 'Slowdive')
