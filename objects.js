b2960b9580317ea5eb2a35b3039782dcea7f2ad3
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-15 07:03:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,3 +11,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName = 'songTitle'
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
