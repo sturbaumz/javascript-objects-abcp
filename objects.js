21fdf950d8a210a74c1d2b4067a9b4a940b771c2
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-15 05:56:16 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,18 @@
+var playlist = {
+  Switchfoot: Gravity,
+  'Florence and the Machine': 'Dog Days'
+}
+
+function updatePlaylist(playlist, artist, song) {
+  // Remember, because one of this function's arguments
+  // is `playlist`, the `playlist` _inside_ the function
+  // is not the same as the top-level `playlist` outside
+  // the function
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
